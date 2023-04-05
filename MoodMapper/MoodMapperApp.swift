9229b0bfc6fefdcd60bae7f5e1e9886c5aff73b5
Morgan <Mharrisstoertz@gmail.com>
2023-04-05 12:01:11 -0400

    Initial Commit

---
+++ MoodMapper/MoodMapperApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodMapperApp.swift
+//  MoodMapper
+//
+//  Created by Morgan Harris-Stoertz on 2023-04-05.
+//
+
+import SwiftUI
+
+@main
+struct MoodMapperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
